c16c7f3b43bded4e70c92c54a8e3428616fd6435
zeenaat13 <zeenat.hussain.1999@gmail.com>
2023-07-12 00:55:16 +0530

    first commit

---
+++ src/Components/InputWithClass.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default class Input extends React.Component{
+
+    render(){
+        return(
+            <>
+            <div className="section">
+                <Row label="Name">
+                        <input className="input"/>
+                </Row >
+                <Row label="Last Name">
+                        <input className="input"/>
+                </Row >
+            </div>
+    
+            <h2>Hello, </h2>
+            
+            </>
+            )
+
+    }
+   
+    }
+
+
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <lable>{label}<br/></lable>
+        {props.children}
+        <hr />
+        </>
+    )
+}
