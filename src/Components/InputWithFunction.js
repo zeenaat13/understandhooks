25cadb3d2386f694174fbc260856799ac49ae657
zeenaat13 <zeenat.hussain.1999@gmail.com>
2023-07-12 12:45:39 +0530

    useState() Hook

--- src/Components/InputWithFunction.js
+++ src/Components/InputWithFunction.js
@@ -1,17 +1,30 @@
+
+import { useState } from "react";
+
 export default function Input(){
+
+    const [name,setName] = useState("Harry");
+    const [lastName,setLastname] = useState("Potter");
+
    
     return(
         <>
         <div className="section">
             <Row label="Name">
-                    <input className="input"/>
+                    <input className="input"
+                            value={name} 
+                            onChange={(e)=> setName(e.target.value)}
+                    />
             </Row >
             <Row label="Last Name">
-                    <input className="input"/>
+                    <input className="input"
+                            value={lastName} 
+                            onChange={(e)=> setLastname(e.target.value)}
+                    />
             </Row >
         </div>
 
-        <h2>Hello, </h2>
+        <h2>Hello, {name + " " +lastName}</h2>
         
         </>
         )
