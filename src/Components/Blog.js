fec44a9d62ae292d69be1a205d853ae36fc5efaa
zeenaat13 <zeenat.hussain.1999@gmail.com>
2023-07-16 19:30:10 +0530

    Adding a new Blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,35 +1,41 @@
+
 //Blogging App using Hooks
+import { useState } from "react";
+
 export default function Blog(){
-    
-    //Passing the synthetic event as argument to stop refreshing the page on submit
+
+    const [title,setTitle] = useState("");
+    const [content,setContent] = useState("");
+    const [blogs, setBlogs] =  useState([]);
+
     function handleSubmit(e){
         e.preventDefault();
+
+        setBlogs([{title,content}, ...blogs]);
+        console.log(blogs);
     }
 
     return(
         <>
-        {/* Heading of the page */}
         <h1>Write a Blog!</h1>
-
-        {/* Division created to provide styling of section to the form */}
         <div className="section">
 
         {/* Form for to write the blog */}
             <form onSubmit={handleSubmit}>
-
-                {/* Row component to create a row for first input field */}
                 <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
+                                placeholder="Enter the Title of the Blog here.."
+                                onChange = {(e) => setTitle(e.target.value)}
+                        />
                 </Row >
 
-                {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                         <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."/>
+                                placeholder="Content of the Blog goes here.."
+                                onChange = {(e) => setContent(e.target.value)}
+                        />
                 </Row >
-
-                {/* Button to submit the blog */}            
+         
                 <button className = "btn">ADD</button>
             </form>
                      
@@ -39,6 +45,13 @@ export default function Blog(){
 
         {/* Section where submitted blogs will be displayed */}
         <h2> Blogs </h2>
+        {blogs.map((blog,i) => (
+            <div className="blog">
+                <h3>{blog.title}</h3>
+                <hr/>
+                <p>{blog.content}</p>
+            </div>
+        ))}
         
         </>
         )
